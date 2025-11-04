be005b13eec8ece8cdea521f49444c8e9d7ce77a
Serhii Kulbashnyi <112683442+Eugensson@users.noreply.github.com>
2025-11-04 09:13:14 +0200

    Initialized project, added assets and styles, installed npm packages

---
+++ app/layout.tsx
@@ -0,0 +1,30 @@
+import type { Metadata } from "next";
+import { Open_Sans } from "next/font/google";
+
+import "./globals.css";
+
+const openSans = Open_Sans({
+  variable: "--font-openSans",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: {
+    default: "Hotely - Book Hotels Worldwide",
+    template: "%s | Hotely",
+  },
+  description:
+    "Discover and book hotels, apartments, and unique stays worldwide. Compare prices, read verified reviews, get instant confirmation and flexible cancellation. Best rates, 24/7 support.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${openSans.variable} antialiased`}>{children}</body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <main className="mx-auto w-full max-w-360 bg-white">
+      <div className="container">homepage</div>
+    </main>
+  );
+};
+
+export default Home;
